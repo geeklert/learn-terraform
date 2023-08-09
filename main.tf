7a236bd4ef9d1c3f241d32ff6ecac3cc3e1c693b
Kuldeep Singh <kuldeep_sing@hcl.com>
2023-08-09 13:34:29 +0530

    adding only main.tf file

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
 provider "aws" {
-  access_key = "AKIAYWQYD6W5VV34XWE4"
-  secret_key = "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"
-  region     = "us-east-1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.region}"
+}
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "region" {
+        default = "us-east-1"
+
 }
 variable "AWS_REGIONS" {
   type = map(string)
